4b8ca7a94b7d0849a3b4a2168c3d0c14b2b6d848
wkcdeie@gmail.com <wkcdeie@gmail.com>
2018-06-19 10:40:59 +0800

    Initial Commit

---
+++ AFNClient/AFNClient.h
@@ -0,0 +1,19 @@
+//
+//  AFNClient.h
+//  AFNClient
+//
+//  Created by golds on 2018/6/19.
+//  Copyright © 2018年 sinsmin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AFNClient.
+FOUNDATION_EXPORT double AFNClientVersionNumber;
+
+//! Project version string for AFNClient.
+FOUNDATION_EXPORT const unsigned char AFNClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AFNClient/PublicHeader.h>
+
+
